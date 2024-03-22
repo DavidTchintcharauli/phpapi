0bde8d1caab0bd621daf92bf01d584225380baa8
DavidTchintcharauli <chincharaulidavid@gmail.com>
2024-03-23 00:54:42 +0400

    fix add_user.php bug

--- api/add_user.php
+++ api/add_user.php
@@ -1,18 +1,23 @@
 <?php
 include 'db_config.php';
 
-$name = $_POST['first_name'];
-$email = $_POST['email'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['first_name'];
+    $email = $_POST['email'];
 
-$sql = "INSERT INTO users (first_name, email) VALUES (?, ?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $first_name, $email);
-if ($stmt->execute()) {
-    echo json_encode(['message' => 'User added successfully']);
+    $sql = "INSERT INTO users (first_name, email) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $name, $email);
+
+    if ($stmt->execute()) {
+        echo json_encode(['message' => 'User added successfully']);
+    } else {
+        echo json_encode(['error' => 'Failed to add user']);
+    }
+
+    $stmt->close();
+    $conn->close();
 } else {
-    echo json_encode(['error' => 'Failed to add user']);
+    echo json_encode(['error' => 'Only POST requests are allowed']);
 }
-
-$stmt->close();
-$conn->close();
 ?>
